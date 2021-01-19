1b2f2c534b19991a35f196f8b0c1425b7298d022
David Thompson <davidbthomp@gmail.com>
2021-01-19 10:07:37 -0600

    Assignment 1

---
+++ web200/Assignment_1/script.js
@@ -0,0 +1,8 @@
+// The variable hello stores the id "hello in the document"
+var hello = document.getElementById("hello");
+
+// This defines the HTML content
+hello.innerHTML="Hello David!";
+
+//This logs to the console, seen on chrome web dev tools
+console.log("Hello David!");
